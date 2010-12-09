b11a7d0e0f100ce1f63c3b825ba1693c6f19a674
Gera Barnabas <quward@gmail.com>
2010-12-09 23:11:17 +0100

    finished tut # 5

--- app/views/pages/warnscript.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-zenity --warning --text="danit leküldeni"
-at -f warnscript.sh now + 5 minutes
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 TutorialFinal::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+  match '/signup',  :to => 'users#new'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/about"
-
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
